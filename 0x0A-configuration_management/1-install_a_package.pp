89af8afed5a0b352f8742741742fa624e97b56d3
[esekyi] <[sskert10@gmail.com]>
2024-07-04 00:22:38 +0000

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
-# install flask using pip3
+# install flask version 2.1.0 using pip3
 
+package {'python3.8':
+  ensure   => '3.8.10',
+  provider => 'pip3',
+}
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
