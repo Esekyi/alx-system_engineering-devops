6916f65d5085ac1a8f0fd47ffeaa7d76a957f750
[esekyi] <[sskert10@gmail.com]>
2024-07-04 00:37:18 +0000

    2. Execute a command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,5 @@
 # execute a command that kills a process named killmenow using pkill
 exec { 'kill_killmenow':
   command => 'pkill -9 killmenow',
+  path    => '/usr/bin/:/usr/local/bin/:/bin/',
 }
