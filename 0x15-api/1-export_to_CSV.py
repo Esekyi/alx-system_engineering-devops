84bdb75282d77e378965e812457a2a185f32da6c
[esekyi] <[sskert10@gmail.com]>
2024-07-30 05:57:22 +0000

    2-export

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""1. Export to CSV"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    employee_ID = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com/users"
+    complete_url = base_url + "/" + employee_ID
+
+    response = requests.get(complete_url)
+    username = response.json().get("username")
+
+    todo_url = complete_url + "/todos"
+    user_todos_response = requests.get(todo_url)
+    todos = user_todos_response.json()
+
+    with open("{}.csv".formart(employee_ID), "w") as file:
+        for task in todos:
+            file.write('"{}","{}","{}","{}"\n'
+                       .format(employee_ID, username, task.get("completed"),
+                               task.get("title")))
