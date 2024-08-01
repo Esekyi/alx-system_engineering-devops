e7b4dac6ca4ae5e4aed75b1077821573ff28cde1
[esekyi] <[sskert10@gmail.com]>
2024-08-01 02:03:31 +0000

    3. Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""3. Dictionary of list of dictionaries
+mandatory
+"""
+
+import json
+import requests
+
+
+if __name__ == "__main__":
+    base_url = "https://jsonplaceholder.typicode.com/users"
+
+    response = requests.get(base_url)
+    data = response.json()
+    for user in data:
+        username= user["username"]
+        employee_ID = user["id"]
+
+        # print(employee_ID)
+        todo_url = base_url + "/{}".format(employee_ID) + "/todos"
+        user_todos_response = requests.get(todo_url)
+        todos = user_todos_response.json()
+        json_format = {employee_ID: []}
+        for task in todos:
+            json_format[employee_ID].append({
+                "username": username,
+                "task": task.get('title'),
+                "completed": task.get("completed")
+			})
+        # print(json_format)
+        with open("todo_all_employees.json", "a") as filename:
+            json.dump(json_format, filename)
+
+
+
+    # for task in todos:
+    #     json_format[employee_ID].append({
+    #            "task": task.get("title"),
+    #            "completed": task.get("completed"),
+    #            "username": username
+    #     })
+    # with open("{}.json".format(employee_ID), "w") as filename:
+    #     json.dump(json_format, filename)
