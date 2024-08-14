b5dd686903adf0a71c65f9479dadbd2b11054b02
[esekyi] <[sskert10@gmail.com]>
2024-08-14 05:16:59 +0000

    fixes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """accessing all subscribers from reddit api"""
 import requests
-from sys import argv
 
 
 def number_of_subscribers(subreddit):
@@ -9,14 +8,15 @@ def number_of_subscribers(subreddit):
     Main function
     make a aget request to redit api to get subscribers
     """
-
-    # subreddit = argv[1]
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, allow_redirects=False, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    try:
+        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        response = requests.get(url, allow_redirects=False, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data['data']['subscribers']
+            return subscribers
+        else:
+            return 0
+    except requests.RequestException as e:
         return 0
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""accessing all first 10 posts from redit"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Main function
+    make a aget request to redit api to get subscribers
+    """
+
+    url = "https://www.reddit.com/r/{}/hot/.json?limit=10".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    try:
+        response = requests.get(url, allow_redirects=False, headers=headers)
+        if response.status_code == 200:
+            data = response.json().get('data', {}).get('children', [])
+            for post in data:
+                print(post['data']['title'])
+        else:
+            print(None)
+    except requests.RequestException as e:
+        print(None)
