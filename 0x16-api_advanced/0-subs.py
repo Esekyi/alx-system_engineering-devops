a88a3e5c9c408b849765d3f1d7ae9241b4f15089
[esekyi] <[sskert10@gmail.com]>
2024-08-15 06:07:54 +0000

    0. subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """accessing all subscribers from reddit api"""
 import requests
+from sys import argv
 
 
 def number_of_subscribers(subreddit):
@@ -8,15 +9,14 @@ def number_of_subscribers(subreddit):
     Main function
     make a aget request to redit api to get subscribers
     """
-    try:
-        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-        headers = {'User-Agent': 'Mozilla/5.0'}
-        response = requests.get(url, allow_redirects=False, headers=headers)
-        if response.status_code == 200:
-            data = response.json()
-            subscribers = data['data']['subscribers']
-            return subscribers
-        else:
-            return 0
-    except requests.RequestException as e:
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, allow_redirects=False, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data.get('data', {}).get('subscribers', 0)
+        return subscribers
+    else:
         return 0
