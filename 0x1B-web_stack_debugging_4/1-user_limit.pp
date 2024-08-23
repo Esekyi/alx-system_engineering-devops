5fe49b90d438fa127854d733fcb7c7a84034498c
[esekyi] <[sskert10@gmail.com]>
2024-08-23 05:48:13 +0000

    1. User limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,7 @@
 # Fix open file limit for holberton user
 
 exec { 'change-os-configuration-for-holberton-user':
-  command => 'sed -i "/holberton hard/d" /etc/security/limits.conf; echo "holberton hard nofile 5000" >> /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/',
+  command  => 'sed -i "/holberton hard/d" /etc/security/limits.conf',
+  path     => '/usr/local/bin/:/bin/',
+  provider => 'shell'
 }
